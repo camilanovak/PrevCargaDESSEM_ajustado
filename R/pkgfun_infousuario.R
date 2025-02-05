7ad45dddf48672a2e0b460d70656a1413983dc25
Camila Novak <CNOVAK@ctgpar.ctgbr.com.br>
2025-02-05 13:37:31 -0300

    Primeiro commit

---
+++ R/pkgfun_infousuario.R
@@ -0,0 +1,7 @@
+cabecalho=function(versao){
+   message("CEPEL - Centro de Pesquisas de Energia Eletrica")
+   message("ONS - Operador Nacional do Sistema Eletrico")
+   message("By cnovak")
+   linha6titulo=paste("VERSAO ",versao,sep="")
+   message(linha6titulo)
+}
